2b05b7cf978a6edac77ad14bf8eed06a75e4320b
amanusk <amanusk@pm.me>
2024-03-10 11:16:43 +0200

    Update to latest next version

--- src/keyDerivation.ts
+++ src/keyDerivation.ts
@@ -5,10 +5,9 @@ import { getStarkKey, utils, grindKey } from "micro-starknet";
 export const baseDerivationPath = "m/44'/9004'/0'/0";
 
 export function getStarkPk(mnemonic: string, index: number): string {
-  const masterNode = ethers.HDNodeWallet.fromMnemonic(ethers.Mnemonic.fromPhrase(mnemonic));
   const fullPath = getPathForIndex(index, baseDerivationPath);
-  const childNode = masterNode.derivePath(fullPath);
-  const groundKey = grindKey(childNode.privateKey);
+  const masterNode = ethers.HDNodeWallet.fromPhrase(mnemonic, undefined, fullPath);
+  const groundKey = grindKey(masterNode.privateKey);
 
   return getStarkKey(groundKey);
 }
